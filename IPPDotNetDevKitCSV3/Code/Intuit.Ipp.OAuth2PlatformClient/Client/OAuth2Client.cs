b42611650fb590006dba3bb439d1c894b13c6e9c
Ivan Kuznetsov <55638302+ikuznetsoft@users.noreply.github.com>
2025-04-01 11:41:00 +0300

    AM-22783: dependabot.yml, nuget update, redundant dependencies removed (#9)
    * AM-22783: dependabot.yml, nuget update, redundant dependencies removed
    
    ---------
    
    Co-authored-by: ikuznetsoft <ivan.kuznetsov@approvalmax.com>

--- IPPDotNetDevKitCSV3/Code/Intuit.Ipp.OAuth2PlatformClient/Client/OAuth2Client.cs
+++ IPPDotNetDevKitCSV3/Code/Intuit.Ipp.OAuth2PlatformClient/Client/OAuth2Client.cs
@@ -19,7 +19,6 @@ using System.IO;
 using System.Globalization;
 using Serilog;
 using Intuit.Ipp.OAuth2PlatformClient.Diagnostics;
-using Serilog;
 
 namespace Intuit.Ipp.OAuth2PlatformClient
 {   /// <summary>
